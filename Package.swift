ccf661a1f67360219c32cfde8412c147effad80b
Jonathan Wight <jwight@mac.com>
2023-09-09 09:26:55 -0700

    Bump min macOS version to 14

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Everything",
     platforms: [
         .iOS(.v15),
-        .macOS(.v12),
+        .macOS(.v14),
         .macCatalyst(.v15),
         .tvOS(.v16),
     ],
